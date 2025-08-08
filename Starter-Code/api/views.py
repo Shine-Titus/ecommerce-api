4475351789719c5fc7d40be2b16086983f26579e
Shine Titus <r.shinetitus@gmail.com>
2025-08-08 22:49:48 +0530

    added views and serializers

--- Starter-Code/api/views.py
+++ Starter-Code/api/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
